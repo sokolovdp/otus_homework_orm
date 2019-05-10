58f5ce558a3078793f72d0d285253c7b6257a14c
dmitrii <sokolovdp@gmail.com>
2019-05-10 12:40:04 +0300

    update ORM

--- sqlite_orm/db_connector.py
+++ sqlite_orm/db_connector.py
@@ -1,6 +1,25 @@
 import sqlite3
 
+from .fields import IntegerField, StringField, DateTimeField
+
 from .exceptions import OrmOperationalError, OrmConfigurationError
+
+
+class BaseSchemaGenerator:
+    TABLE_CREATE_TEMPLATE = "CREATE TABLE {exists} '{table_name}' ({fields});"
+    FIELD_TEMPLATE = '"{name}" {db_type} {nullable}'
+
+    FIELD_TYPE_MAP = {
+        IntegerField: "BIGINT",
+        StringField: "VARCHAR({})",
+        DateTimeField: "TIMESTAMP",
+    }
+
+
+class SqliteSchemaGenerator(BaseSchemaGenerator):
+    def __init__(self, *args, **kwargs) -> None:
+        super().__init__(*args, **kwargs)
+
 
 
 class SQLite:
